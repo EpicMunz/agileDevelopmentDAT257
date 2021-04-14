6b42ada8b2357d733c930da820a2c1564ddbf915
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-04-14 14:16:58 +0200

    Installed Bootstrap and added our first component

---
+++ my-app/src/components/InteractiveMap.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default class InteractiveMap extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1 className="badge badge-primary">This is a map</h1>
+      </React.Fragment>
+    );
+  }
+}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import InteractiveMap from './components/InteractiveMap'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <InteractiveMap/>,
   document.getElementById('root')
 );
 
