45ea99f6dd0cc5434df12bab393318fff8a5e4ef
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-04-14 15:14:07 +0200

    cleaned up whitespaces

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import InteractiveMap from './components/InteractiveMap'
-import MenuHeader from './components/MenuHeader'
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import InteractiveMap from "./components/InteractiveMap";
+import MenuHeader from "./components/MenuHeader";
 
 function App() {
   return (
     <div>
-        <MenuHeader/>
-        <InteractiveMap/>
+      <MenuHeader />
+      <InteractiveMap />
     </div>
   );
 }
--- my-app/src/components/InteractiveMap.jsx
+++ my-app/src/components/InteractiveMap.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 export default class InteractiveMap extends Component {
-
   render() {
     return (
       <div className="text-center">
-        <h1 className="badge badge-primary my-5" >This is a map</h1>
+        <h1 className="badge badge-primary my-5">This is a map</h1>
       </div>
     );
   }
