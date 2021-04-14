ba1f1282abe33c3804177e0b70af56701bad0f86
Zneed99 <holmesten99@hotmail.com>
2021-04-14 16:44:19 +0200

    We have a big map over Chalmers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import InteractiveMap from "./components/InteractiveMap";
 import MenuHeader from "./components/MenuHeader";
 
+//This is the file for App Component. App Component is the main component in React which acts as a container for all other components.
+
 function App() {
   return (
     <div>
--- my-app/src/components/InteractiveMap.jsx
+++ my-app/src/components/InteractiveMap.jsx
@@ -1,12 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import ReactMapGl from "react-map-gl"
+
 
 export default class InteractiveMap extends Component {
-  render() {
-    return (
-      <div className="text-center">
-        <h1 className="badge badge-primary my-5">This is a map</h1>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = { viewport: {
+            latitude: 57.688610,
+            longitude: 11.978938,
+            width: "100vw",
+            height: "100vh",
+            zoom: 16
+            }
+        };
+    }
+
+    render() {
+        return (
+        <div>
+            <ReactMapGl {...this.state.viewport}
+            mapboxApiAccessToken={"pk.eyJ1Ijoiem5lZWQiLCJhIjoiY2tuaGpydWFqM2ZqMDJvbng5MHRudGY4OSJ9.D11Dq-An0CoSaUa4JPzQbQ"}>
+            </ReactMapGl>
+        </div>
+        );
+      }
 }
