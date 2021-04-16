9cf92202b97b5cdc68824aadbc6c0e9ed89cd7a7
Felix Nilsson <felix.nilsson.se@gmail.com>
2021-04-16 17:47:13 +0200

    Gjorde header
    Fattas fortfarande navigering och lite styling.

--- my-app/src/components/MenuHeader.jsx
+++ my-app/src/components/MenuHeader.jsx
@@ -1,8 +1,9 @@
 import "antd/dist/antd.css";
 import React, { Component } from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu} from "antd";
 
 const { Header } = Layout;
+const { SubMenu } = Menu;
 
 export default class MenuHeader extends Component {
   render() {
@@ -11,9 +12,21 @@ export default class MenuHeader extends Component {
         <Header>
           <div className="logo" />
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+            <img
+              src={`${process.env.PUBLIC_URL}indek.png`}     
+              width="50"
+              height="50"
+
+              alt = "sektionslogga"
+            />
+            <SubMenu key="sub1"  title="Profil"> 
+              <Menu.ItemGroup key="g1">
+              <Menu.Item key="3">Användare</Menu.Item>
+              <Menu.Item key="4">Logga ut</Menu.Item>
+          </Menu.ItemGroup>
+            </SubMenu>
+            <Menu.Item key="1">Startsida</Menu.Item>
+            <Menu.Item key="2">Karta</Menu.Item>
           </Menu>
         </Header>
       </Layout>
