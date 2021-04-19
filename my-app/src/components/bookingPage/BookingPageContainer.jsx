24239eb9d31cf8a1f62e247f430521f0bd1ec441
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-04-19 17:08:09 +0200

    Worked on mybookings page

--- my-app/src/components/bookingPage/BookingPageContainer.jsx
+++ my-app/src/components/bookingPage/BookingPageContainer.jsx
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Divider } from "antd";
+import "./BookingPageContainer.css"
 
 const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 export default class BookingPageContainer extends Component {
   state = {};
   render() {
     return (
-      <div className="container">
-        <h1>Title</h1>
-        <Layout.Sider
-          className="sidebar"
+      <Layout>
+        <Sider
+          className="site-layout-background"
           breakpoint={"lg"}
           theme="light"
           trigger={null}
         >
           <Menu
-            style={{ width: 256 }}
+            style={{ height: "100%", borderRight: 0 }}
             defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
             mode="inline"
           >
             <Menu.Item key="1">Geniknölen</Menu.Item>
@@ -26,8 +26,16 @@ export default class BookingPageContainer extends Component {
             <Menu.Item key="3">Vasaparken</Menu.Item>
             <Menu.Item key="4">Deltaparken</Menu.Item>
           </Menu>
-        </Layout.Sider>
-      </div>
+        </Sider>
+        <Content className="site-layout-background" style={{ padding: 24, margin: 0 }}>
+          <div className="row" style={{fontWeight: "bold"}}>
+              <div className="col">Event</div>
+              <div className="col">Datum</div>
+              <div className="col">Tid</div>
+          </div>
+          <Divider />
+        </Content>
+      </Layout>
     );
   }
 }
