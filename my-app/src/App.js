81360f6eab6ff6a517820aeac0a7234ae5abde78
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-04-19 15:41:07 +0200

    fixed previous commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import InteractiveMap from "./components/InteractiveMap";
 import MenuHeader from "./components/MenuHeader";
 import Schedule from "./components/Schedule";
-import bookingPageContainer from "./components/bookingPage/bookingPageContainer";
+import BookingPageContainer from "./components/bookingPage/BookingPageContainer";
 
 //This is the file for App Component. App Component is the main component in React which acts as a container for all other components.
 
