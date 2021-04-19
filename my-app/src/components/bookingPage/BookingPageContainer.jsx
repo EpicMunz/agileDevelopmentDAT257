5ac95b757c4806308af97ba5cecac4e1b30bf1b5
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-04-19 15:39:33 +0200

    Revert "Revert "created container for booking page and renderer it in app""
    This reverts commit 39819a477143a2ff126ca38e35b753e99d0b56bb.

---
+++ my-app/src/components/bookingPage/BookingPageContainer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class BookingPageContainer extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Title</h1> );
+    }
+}
+ 
