886cd87a530cfa49dfc4c8c5394ee4e96023842c
axelwes <axelwes@student.chalmers.se>
2021-04-24 19:00:54 +0200

    Login box
    Hardcoded user login check. Commented out red button.

--- my-app/src/components/LogInPage.jsx
+++ my-app/src/components/LogInPage.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import InteractiveMap from "./InteractiveMap";
+import "./LoginPage.css";
 
 export default class LogInPage extends Component {
   	constructor(props){
 		super(props);
-		this.state = { isActive: false};
+		this.state = { isActive: false,
+		username: "", password: ""};
 		this.handleClick = this.handleClick.bind(this);
+		this.handleLogin = this.handleLogin.bind(this);
 	}
 
 	handleClick() {
@@ -13,17 +16,45 @@ export default class LogInPage extends Component {
 		this.props.onDisplayChange(InteractiveMap);
 	}
 
+	//Kolla console log F12
+
+	handleLogin() {
+		if(this.state.username === "mottagningskommitten" && 
+		this.state.password === "password"){
+			console.log(this.state);
+			this.props.onDisplayChange(InteractiveMap); 
+		}
+		else{
+			console.log("Fel");
+		}
+	}
+
 	render(){
 		return(
 			<div>
-				<button 
+				{/*<button 
 					className="marker-btn"
 					onClick={this.handleClick}>
 					<img src="redButton.png" alt="Premise button" />
 				</button>
+				*/}
+
+				<div className="username-box">
+						<label for="username">Username:</label>
+						<input type="text" id="username" name="login" 
+						onChange={e=>this.setState({username:e.target.value})} className="input-box"/><br/><br/>
+						<label for="password">Password:</label>
+						<input type="password" id="password" name="login" 
+						onChange={e=>this.setState({password:e.target.value})}className="input-box"/><br/><br/>
+						<button onClick={this.handleLogin}> Submit </button> 
+				</div>
+
+
 			</div>
+
 		);
     }
+
+
+
 }
-
-
