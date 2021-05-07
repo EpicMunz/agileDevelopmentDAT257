624dd94de60aa2500779364b18a439ff190c5e90
sarasehdo <71220961+sarasehdo@users.noreply.github.com>
2021-05-07 12:18:11 +0200

    Styling of UserPage

--- my-app/src/components/profilePage/EditProfile.jsx
+++ my-app/src/components/profilePage/EditProfile.jsx
@@ -1,40 +1,57 @@
 import { message } from "antd";
 import {Component} from "react";
-//import {fetchData} from "../clientFetch/ClientFetch";
+import {fetchData} from "../clientFetch/ClientFetch";
 import "./ManageProfilePage.jsx";
+import "./EditProfile.css";
+//import pen from "./pen.png";
+
+var bcrypt = require('bcryptjs');
 
 export default class EditProfile extends Component{
 
     constructor(props){
         super(props)
         this.state = { isActive: false,
-            username: "", email: "", password: "", disabledEmail: true, disabledPassword: true, disabledNewPassword: true};         //byt till disabled istället för show
-        this.handleChangePassword = this.handleChangePassword.bind(this);
-        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+            disabledEmail: true, disabledPassword: true, disabledNewPassword: true};         //byt till disabled istället för show, username: "", email: "", password: "",
+        /*this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);*/
         this.handleEmail = this.handleEmail.bind(this);
         this.handlePassword = this.handlePassword.bind(this);
         
     }
-
-
     //Changes state values when textfield changes
-    handleChangePassword(e){
+   /* handleChangePassword(e){
         this.setState({password: e.target.value});
     }
     handleChangeEmail(e){
         this.setState({email: e.target.value});
-    }
+    }*/
 
     handleEmail(){
         this.setState({disabledEmail: !this.state.disabledEmail});
     }
-
     handlePassword(){
-        
         this.setState({disabledPassword: !this.state.disabledPassword});
         this.setState({disabledNewPassword: !this.state.disabledNewPassword});
     }
+    handleSave(event){
+        event.preventDefault();
+        var encryptedPassword = bcrypt.hashSync(this.state.password,10);
 
+        var data = {
+            Password: encryptedPassword,
+            Status: "User",
+            Mail: this.state.email
+        }
+        //Requests adding of user
+        fetchData("/editProfile", data);
+        document.getElementById("formdata").reset();
+        alert("Nya detaljer är sparade");
+        this.props.onSubmit();
+
+    }
+
+    //Updates userData whenever you login to ManageProfilePage
     componentDidMount(){
         var fetchedData = JSON.parse(sessionStorage.getItem("userData"));     
         console.log(fetchedData.Username);   
@@ -47,35 +64,43 @@ export default class EditProfile extends Component{
     //Box of form
         render(){
             return (<div className="rectangleSave">
-            <h3>Användarprofil</h3>
+            <h3 className="rubrik" >Användarprofil</h3>
             <form id="saveform" onSubmit={(e) => this.handleSave(e)}>
                     <div>
-                        <label style={{backgroundColor: "lightblue"}} type="text" name="name"> 
+                        <label className="alignment" type="text" name="name"> 
                         Användarnamn: 
-                        {this.state.username}
                         </label>
+                        <div><label className="username" type="text" name="name"> {this.state.username} </label></div>
                     </div>
                     <div>
-                        <label type="text" name="name">
-                        Email:
+                        <label className="alignment" type="text" name="name">
+                            Email:
                         </label>
-                        <input placeholder={this.state.email} type="text" name="name" disabled={this.state.disabledEmail}/>
-                        <image style={{backgroundColor:"lightblue"}} onClick={this.handleEmail}> byt email </image>             
+                        <div>
+                            <input className= "input" placeholder={this.state.email} type="text" name="name" disabled={this.state.disabledEmail}/>
+                            <img className="pen-logo" src={`${process.env.PUBLIC_URL}pen.png`} alt="edit" onClick={this.handleEmail}/>
+                        </div>    
                     </div>
                     <div>
-                        <label type="password" name="password">
+                        <label className="alignment" type="password" name="password">
                         Lösenord:
                         </label>
-                        <input placeholder="gammalt lösenord" type="password" name="name" disabled={this.state.disabledPassword}/>   
-                        <image style={{backgroundColor:"lightblue"}} onClick={this.handlePassword}> byt lösenord </image>     
+                        <div>
+                            <input className="input" placeholder="nuvarande lösenord" type="password" name="name" disabled={this.state.disabledPassword}/>   
+                            <img className="pen-logo" src={`${process.env.PUBLIC_URL}pen.png`} alt="edit" onClick={this.handlePassword} />  
+                        </div>
                     </div>
                     <div>
-                        <label type="password" name="password">
+                        <label className="alignment" type="password" name="password">
                         Nytt lösenord:
                         </label>
-                        <input placeholder="nytt lösenord" type="password" name="name" disabled={this.state.disabledNewPassword}/>      
+                        <div>
+                            <input className="input" placeholder="nytt lösenord" type="password" name="name" disabled={this.state.disabledNewPassword}/>  
+                        </div>   
                     </div>
-                    <input type="submit" value="Spara" />
+                    <div className="save-button">
+                        <input type="submit" value="Spara"/>
+                    </div>
             </form></div>);
     
         }
