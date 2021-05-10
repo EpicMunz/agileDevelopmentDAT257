14184200e720373bf0c911e25093f99e40dad861
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-05-10 14:48:27 +0200

    removed binds on loginpage

--- my-app/src/components/logInPage/ForgotPassword.jsx
+++ my-app/src/components/logInPage/ForgotPassword.jsx
@@ -1,40 +1,33 @@
 import React, { Component } from "react";
-import { Form, Input, Button} from "antd";
+import { Form, Input, Button } from "antd";
 import { fetchData } from "../clientFetch/ClientFetch";
-import LoginPage from "./LogInPage.jsx"
+import LoginPage from "./LogInPage.jsx";
 
 export default class ForgotPassword extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onBack = this.onBack.bind(this);
-  }
+  state = {};
 
-/*
+  /*
 Reacts to the button submit being pressed
 sends an email to the mail address given, if it exists in the database
 */
- async onSubmit(){
-    var data = { Mail:this.state.adress}
-    var response = await fetchData("/getUsersMail",data); 
+  onSubmit = async () => {
+    var data = { Mail: this.state.adress };
+    var response = await fetchData("/getUsersMail", data);
     var ofund = await response.json();
-    if (ofund.Response === "Mail has been sent"){
-        alert("Mail skickat");
-    }else{
+    if (ofund.Response === "Mail has been sent") {
+      alert("Mail skickat");
+    } else {
       alert("Mail hittades inte");
     }
-    
-    
-}
-  
-onChange = (e) => {
-	this.setState({adress: e.target.value});
-}
+  };
 
-onBack = (e) =>{
-  this.props.onDisplayChange(LoginPage);
-}
+  onChange = (e) => {
+    this.setState({ adress: e.target.value });
+  };
+
+  onBack = (e) => {
+    this.props.onDisplayChange(LoginPage);
+  };
 
   render() {
     return (
@@ -53,21 +46,17 @@ onBack = (e) =>{
             },
           ]}
         >
-          <Input value={this.state.adress} onChange = {this.onChange} />
+          <Input value={this.state.adress} onChange={this.onChange} />
         </Form.Item>
 
         <Form.Item>
-          <Button type="primary" htmlType="submit" onClick = {this.onSubmit}>
+          <Button type="primary" htmlType="submit" onClick={this.onSubmit}>
             Submit
           </Button>
-          <Button type="primary" htmlType="submit" onClick = {this.onBack}>
+          <Button type="primary" htmlType="submit" onClick={this.onBack}>
             Back
           </Button>
-          
         </Form.Item>
-
-        
-        
       </Form>
     );
   }
--- my-app/src/components/logInPage/LogInPage.jsx
+++ my-app/src/components/logInPage/LogInPage.jsx
@@ -1,82 +1,80 @@
 import React, { Component } from "react";
 import InteractiveMap from "../map/InteractiveMap";
 import "./LoginPage.css";
-import {fetchData} from "../clientFetch/ClientFetch";
+import { fetchData } from "../clientFetch/ClientFetch";
 import ForgotPassword from "./ForgotPassword.jsx";
-import { Form, Input, Button, Checkbox } from 'antd';
-
-
+import { Form, Input, Button, Checkbox } from "antd";
 
 export default class LogInPage extends Component {
-  	constructor(props){
-		super(props);
-		this.state = { isActive: false,
-		username: "", password: ""};
-		this.handleLogin = this.handleLogin.bind(this);
-	}
+  state = { isActive: false, username: "", password: "" };
+ 
+  //checks if username and password was correct, if yes then switch to map screen
+  handleLogin = async () => {
+    if (await this.getData()) {
+      this.props.onDisplayChange(InteractiveMap);
+    } else {
+      alert("Wrong Username or Password!");
+    }
+  }
+  //Asks server for userdata related to the username and password combination
+  async getData() {
+    var data = [
+      {
+        Username: this.state.username,
+        Password: this.state.password,
+      },
+    ];
+    var receivedData = await fetchData("/logInUser", data);
+    var jsonData = await receivedData.json();
+    if (jsonData != null && jsonData.Username != null) {
+      sessionStorage.setItem("userData", JSON.stringify(jsonData));
+      return true;
+    }
+    return false;
+  }
 
-   //checks if username and password was correct, if yes then switch to map screen
-	async handleLogin() {
-		if(await this.getData()){
-			this.props.onDisplayChange(InteractiveMap);
-		}
-		else{
-			alert("Wrong Username or Password!");
-		}
-	}
-	//Asks server for userdata related to the username and password combination
-	async getData(){
-          var data = [{
-              Username: this.state.username,
-              Password: this.state.password
-          }];
-          var receivedData = await fetchData("/logInUser", data);
-          var jsonData = await receivedData.json();
-          if(jsonData != null && jsonData.Username != null){
-            sessionStorage.setItem("userData", JSON.stringify(jsonData));
-            return true;
-          }
-          return false;
-      }
+  onChange = (e) => {
+    this.props.onDisplayChange(ForgotPassword);
+  };
 
-	onChange = (e) =>{
-		this.props.onDisplayChange(ForgotPassword);
-	}
-
-	render(){
-
-
-		return(
-        <div>
-        <img className="imgCenter" src={`${process.env.PUBLIC_URL}MKloginlogga.png`} alt="Avatar"
-              width="500"
-              height="400"
+  render() {
+    return (
+      <div>
+        <img
+          className="imgCenter"
+          src={`${process.env.PUBLIC_URL}MKloginlogga.png`}
+          alt="Avatar"
+          width="500"
+          height="400"
         />
-        <Form className="item"
+        <Form
+          className="item"
           name="basic"
           initialValues={{
             remember: true,
           }}
         >
-          <Form.Item onChange={e=>this.setState({username:e.target.value})}
+          <Form.Item
+            onChange={(e) => this.setState({ username: e.target.value })}
             label="Username"
             name="username"
             rules={[
               {
                 required: true,
-                message: 'Please input your username!',
+                message: "Please input your username!",
               },
             ]}
           >
             <Input />
           </Form.Item>
-          <Form.Item onChange={e=>this.setState({password:e.target.value})}
+          <Form.Item
+            onChange={(e) => this.setState({ password: e.target.value })}
             label="Password"
             name="password"
             rules={[
               {
                 required: true,
-                message: 'Please input your password!',
+                message: "Please input your password!",
               },
             ]}
           >
@@ -85,18 +83,16 @@ export default class LogInPage extends Component {
           <Form.Item name="remember" valuePropName="checked">
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
-          <Form.Item >
+          <Form.Item>
             <Button type="primary" htmlType="submit" onClick={this.handleLogin}>
               Submit
             </Button>
-            	<Button
-                        size= "small"
-                        type="link"
-                        onClick={this.onChange}>
-                            Glömt mitt lösenord</Button>
+            <Button size="small" type="link" onClick={this.onChange}>
+              Glömt mitt lösenord
+            </Button>
           </Form.Item>
         </Form>
-        </div>
-      );
-    }
+      </div>
+    );
+  }
 }
