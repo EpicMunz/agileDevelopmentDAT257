05cc4cd53e98577486d74811802c28ef85c9b108
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-05-10 14:42:29 +0200

    removed binds for map

--- my-app/src/components/map/InteractiveMap.jsx
+++ my-app/src/components/map/InteractiveMap.jsx
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import ReactMapGl, { Marker, Popup } from "react-map-gl";
 import NewLocation from "./NewMapLocation.jsx";
-import {fetchData } from "../clientFetch/ClientFetch.jsx";
+import { fetchData } from "../clientFetch/ClientFetch.jsx";
 
 export default class InteractiveMap extends Component {
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.getData();
-    this.handleRemoveLocation = this.handleRemoveLocation.bind(this);
   }
   state = {
     //This makes it so that the map is created with a focus over Chalmers university
@@ -26,124 +24,122 @@ export default class InteractiveMap extends Component {
   };
   //Sends a request to the server for the data of all locations
   async getData() {
-        var data = [
-          {
-            Id: 1,
-          },
-        ];
-        const api_call = await fetchData("/getLocations", data);
-        const response = await api_call.json();
-        this.setState({data: response, newLocation: null, selectedPremise: null});
-    }
+    var data = [
+      {
+        Id: 1,
+      },
+    ];
+    const api_call = await fetchData("/getLocations", data);
+    const response = await api_call.json();
+    this.setState({ data: response, newLocation: null, selectedPremise: null });
+  }
 
   handleClick = (location) => {
     this.props.onLocationChange(location);
   };
   //Sends locationData of the location that is to be removed to the server
-  async handleRemoveLocation(locationName){
-      var data = {
-        name: locationName
-      }
-      await fetchData("/removeLocation", data);
-      this.getData();
-
-  }
+  handleRemoveLocation = async (locationName) => {
+    var data = {
+      name: locationName,
+    };
+    await fetchData("/removeLocation", data);
+    this.getData();
+  };
   //On single click on empty spot on map
-  onMapClick(locationData){
-    if(JSON.parse(sessionStorage.getItem("userData")).Status === "admin"){
-        this.setState({newLocation: locationData});
+  onMapClick(locationData) {
+    if (JSON.parse(sessionStorage.getItem("userData")).Status === "admin") {
+      this.setState({ newLocation: locationData });
     }
-    this.setState({selectedPremise: null});
+    this.setState({ selectedPremise: null });
   }
 
   render() {
+    return this.state.data != null ? (
+      <div>
+        <ReactMapGl
+          {...this.state.viewport}
+          //Restyles the map, this one is called satellite but there are many styles to chose from
 
-        return this.state.data != null ? (
+          //Basic view
+          mapStyle="mapbox://styles/zneed/cknob180n4u8p17ny8h3qm0xr"
+          //This accesses the mapBox(google map) token
+          mapboxApiAccessToken={
+            "pk.eyJ1Ijoiem5lZWQiLCJhIjoiY2tuaGpydWFqM2ZqMDJvbng5MHRudGY4OSJ9.D11Dq-An0CoSaUa4JPzQbQ"
+          }
+          //When someone interacts with the map it sends a new viewport back so that the variables are changed. With this you can therefore move the map
+          onViewportChange={(viewport) => {
+            this.setState({ viewport });
+          }}
+          onClick={(element) => {
+            this.onMapClick(element);
+          }}
+        >
+          {/*Shows our locations on the map with a button, created an event on the button to make it interactable*/}
+          {this.state.data.features.map((premises) => (
+            <Marker
+              key={premises.properties.LOCATION_ID}
+              latitude={premises.geometry.coordinates[0]}
+              longitude={premises.geometry.coordinates[1]}
+            >
+              <button
+                className="marker-btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.setState({ selectedPremise: premises });
+                  this.setState({ newLocation: null });
+                }}
+              >
+                <img src="redButton.png" alt="Premise button" />
+              </button>
+            </Marker>
+          ))}
+          {/*If a button is clicked a popup window is created at that location on the map*/}
+          {this.state.selectedPremise ? (
+            <Popup
+              latitude={this.state.selectedPremise.geometry.coordinates[0]}
+              longitude={this.state.selectedPremise.geometry.coordinates[1]}
+            >
               <div>
-                <ReactMapGl
-                  {...this.state.viewport}
-                  //Restyles the map, this one is called satellite but there are many styles to chose from
-
-                  //Basic view
-                  mapStyle="mapbox://styles/zneed/cknob180n4u8p17ny8h3qm0xr"
-                  //This accesses the mapBox(google map) token
-                  mapboxApiAccessToken={
-                    "pk.eyJ1Ijoiem5lZWQiLCJhIjoiY2tuaGpydWFqM2ZqMDJvbng5MHRudGY4OSJ9.D11Dq-An0CoSaUa4JPzQbQ"
+                <h2>{this.state.selectedPremise.properties.NAME}</h2>
+                <p>{this.state.selectedPremise.properties.NOTES} </p>
+                <p>{this.state.selectedPremise.properties.FACILITY}</p>
+                <button
+                  className="boka-tid-knapp"
+                  onClick={() =>
+                    this.handleClick(this.state.selectedPremise.properties.NAME)
                   }
-                  //When someone interacts with the map it sends a new viewport back so that the variables are changed. With this you can therefore move the map
-                  onViewportChange={(viewport) => {
-                    this.setState({ viewport });
-                  }}
-                  onClick={(element) => {
-                    this.onMapClick(element);
-                  }}
                 >
-                  {/*Shows our locations on the map with a button, created an event on the button to make it interactable*/}
-                  {this.state.data.features.map((premises) => (
-                    <Marker
-                      key={premises.properties.LOCATION_ID}
-                      latitude={premises.geometry.coordinates[0]}
-                      longitude={premises.geometry.coordinates[1]}
-                    >
-                      <button
-                        className="marker-btn"
-                        onClick={(e) => {
-                          e.preventDefault();
-                          this.setState({ selectedPremise: premises });
-                          this.setState({ newLocation: null});
-                        }}
-                      >
-                        <img src="redButton.png" alt="Premise button" />
-                      </button>
-                    </Marker>
-                  ))}
-                  {/*If a button is clicked a popup window is created at that location on the map*/}
-                  {this.state.selectedPremise ? (
-                    <Popup
-                      latitude={this.state.selectedPremise.geometry.coordinates[0]}
-                      longitude={this.state.selectedPremise.geometry.coordinates[1]}
-                    >
-                      <div>
-                        <h2>{this.state.selectedPremise.properties.NAME}</h2>
-                        <p>{this.state.selectedPremise.properties.NOTES} </p>
-                        <p>{this.state.selectedPremise.properties.FACILITY}</p>
-                        <button
-                          className="boka-tid-knapp"
-                          onClick={() =>
-                            this.handleClick(this.state.selectedPremise.properties.NAME)
-                          }
-                        >
-                          Boka tid
-                        </button>
-                        {JSON.parse(sessionStorage.getItem("userData")).Status === "admin" ? (
-                            <button
-                                  className="boka-tid-knapp"
-                                  onClick={() =>
-                                    this.handleRemoveLocation(this.state.selectedPremise.properties.NAME)
-                                  }
-                                >
-                                  Ta bort plats
-                            </button>
-
-                        ): null}
-                      </div>
-                    </Popup>
-
-                  ) : null}
-                  {this.state.newLocation != null ? (
-
-                      <Popup
-                        latitude={this.state.newLocation.lngLat[1]}
-                        longitude={this.state.newLocation.lngLat[0]}
-                      ><NewLocation
-                        coordinates = {this.state.newLocation}
-                        onChange={() => this.getData()}
-                      />
-                      </Popup>
-
-                    ) :null}
-                </ReactMapGl>
+                  Boka tid
+                </button>
+                {JSON.parse(sessionStorage.getItem("userData")).Status ===
+                "admin" ? (
+                  <button
+                    className="boka-tid-knapp"
+                    onClick={() =>
+                      this.handleRemoveLocation(
+                        this.state.selectedPremise.properties.NAME
+                      )
+                    }
+                  >
+                    Ta bort plats
+                  </button>
+                ) : null}
               </div>
-            ) : null;
+            </Popup>
+          ) : null}
+          {this.state.newLocation != null ? (
+            <Popup
+              latitude={this.state.newLocation.lngLat[1]}
+              longitude={this.state.newLocation.lngLat[0]}
+            >
+              <NewLocation
+                coordinates={this.state.newLocation}
+                onChange={() => this.getData()}
+              />
+            </Popup>
+          ) : null}
+        </ReactMapGl>
+      </div>
+    ) : null;
   }
 }
--- my-app/src/components/map/NewMapLocation.jsx
+++ my-app/src/components/map/NewMapLocation.jsx
@@ -1,63 +1,68 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./NewMapLocation.css";
-import {fetchData} from "../clientFetch/ClientFetch.jsx";
-
+import { fetchData } from "../clientFetch/ClientFetch.jsx";
 
 export default class NewMapLocation extends Component {
+  state = { name: "", description: "" };
 
-
-    constructor(props){
-        super(props)
-        this.state = {name: "", description: ""};
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-    }
-
-    handleNameChange(e){
-        this.setState({name: e.target.value});
-    }
-    handleDescriptionChange(e){
-        this.setState({description: e.target.value});
-    }
-    //Send new location data to server
-    async handleSubmit(event){
-        event.preventDefault();
-        var data = {
-                   			"type": "Feature",
-                   			"properties": {
-                   				"LOCATION_ID": null,
-                   				"NAME": this.state.name,
-                   				"NOTES": "Outdoor",
-                   				"FACILITY": this.state.description
-                   			},
-                   			"geometry": {
-                   				"type": "Point",
-                   				"coordinates": [
-                   					this.props.coordinates.lngLat[1],
-                   					this.props.coordinates.lngLat[0]
-                   				]
-                   			}
-                   		}
-        await fetchData("/addLocation", data);
-        this.props.onChange();
-    }
-    //renders add location popup on map
-    render(){
-
-        return (
-        <div className="rectangleNewLocation">
-            <h2>Lägg till Plats</h2>
-            <form id="formdata" onSubmit={(e) => this.handleSubmit(e)}>
-                  <label>
-                          Name of location:
-                          <input type="text" name="name" value={this.state.name} onChange= {this.handleNameChange}/>
-                        </label>
-                        <label>
-                          Description:
-                         <textarea name="description" value={this.state.description} onChange= {this.handleDescriptionChange}/>
-                  </label>
-                  <input className="boka-tid-knapp" type="submit" value="Lägg till plats" />
-            </form>
-        </div>);
-    }
+  handleNameChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
+  handleDescriptionChange = (e) => {
+    this.setState({ description: e.target.value });
+  };
+  //Send new location data to server
+  async handleSubmit(event) {
+    event.preventDefault();
+    var data = {
+      type: "Feature",
+      properties: {
+        LOCATION_ID: null,
+        NAME: this.state.name,
+        NOTES: "Outdoor",
+        FACILITY: this.state.description,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          this.props.coordinates.lngLat[1],
+          this.props.coordinates.lngLat[0],
+        ],
+      },
+    };
+    await fetchData("/addLocation", data);
+    this.props.onChange();
+  }
+  //renders add location popup on map
+  render() {
+    return (
+      <div className="rectangleNewLocation">
+        <h2>Lägg till Plats</h2>
+        <form id="formdata" onSubmit={(e) => this.handleSubmit(e)}>
+          <label>
+            Name of location:
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+            />
+          </label>
+          <label>
+            Description:
+            <textarea
+              name="description"
+              value={this.state.description}
+              onChange={this.handleDescriptionChange}
+            />
+          </label>
+          <input
+            className="boka-tid-knapp"
+            type="submit"
+            value="Lägg till plats"
+          />
+        </form>
+      </div>
+    );
+  }
 }
