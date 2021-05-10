c95dbc1037597272bf2afd3082f4137bfa6c47a6
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-05-10 14:33:28 +0200

    fixed typo in classname and removed constructor and binds

--- my-app/src/components/profilePage/ManageProfilePage.jsx
+++ my-app/src/components/profilePage/ManageProfilePage.jsx
@@ -1,16 +1,11 @@
 import { Component } from "react";
 import EditProfile from "./EditProfile.jsx";
 
-export default class ManageProfileProfile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { action: false };
-    this.actionHasBeenTaken = this.actionHasBeenTaken.bind(this);
-  }
-
-  actionHasBeenTaken() {
+export default class ManageProfilePage extends Component {
+  state = { action: false };
+  actionHasBeenTaken = () => {
     this.setState({ action: !this.state.action });
-  }
+  };
 
   render() {
     return (
