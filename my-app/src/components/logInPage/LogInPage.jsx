8b7ef71384258f361fc7606322d279543fee8c3f
Zneed99 <holmesten99@hotmail.com>
2021-05-11 18:08:25 +0200

    Made available locations and forgot password pages more consistent with antd

--- my-app/src/components/logInPage/LogInPage.jsx
+++ my-app/src/components/logInPage/LogInPage.jsx
@@ -48,7 +48,7 @@ export default class LogInPage extends Component {
           height="400"
         />
         <Form
-          className="item"
+          className="items"
           name="basic"
           initialValues={{
             remember: true,
